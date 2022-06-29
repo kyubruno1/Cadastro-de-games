be9d1eaf0769f01227c7974cd30d5b658d9ccdef
Bruno Ribeiro <kyubruno@gmail.com>
2022-06-29 16:44:06 -0300

    criaçao de novas rotas e metodo no controller

--- controllers/GamesController.js
+++ controllers/GamesController.js
@@ -7,4 +7,27 @@ module.exports = class GamesController {
             allGames: allGames
         })
     }
+
+    static async create(req, res){
+        const {name, category} = req.body
+        let concluded = req.body.concluded
+      
+        if(concluded === '0') {
+            concluded = false
+        } else {
+            concluded = true
+        }
+
+        await Games.create({name, category, concluded})
+
+        res.status(200).json({message: 'Jogo salvo na base de dados!'})
+    }
+
+    static async delete(req, res){
+        const id = req.params.id 
+
+        await Games.destroy({where: {id:id}})
+
+        res.status(200).json({message: 'Jogo deletado da base de dados'})
+    }
 }
--- routes/GamesRoutes.js
+++ routes/GamesRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const GamesController = require('../controllers/GamesController')
 
 router.get('/', GamesController.getAll)
+router.post('/create', GamesController.create)
+router.post('/delete/:id', GamesController.delete)
 
 
 module.exports = router
